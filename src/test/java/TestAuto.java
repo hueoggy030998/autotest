a14d82149a3ecf45bc370dbce1671e9e5a990570
hueoggy030998 <huetransky@gmail.com>
2023-12-13 18:03:38 +0700

    annotation

--- src/test/java/TestAuto.java
+++ src/test/java/TestAuto.java
@@ -1,29 +1,54 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
+import java.io.IOException;
 import java.time.Duration;
 
 public class TestAuto {
-    @Test
-    public static void checkPage(){
-        WebDriver driver;
+    WebDriver driver;
+    @BeforeClass
+    public void openChrome(){
         WebDriverManager.chromedriver().setup();
         driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.get("https://test-aitriage-web.beedu.vn/login");
-        Assert.assertEquals(driver.getTitle(),"aiTriage");
+    }
+    @AfterClass
+    public void closeChrome(){
         driver.close();
         driver.quit();
     }
 
-    public static void main(String[] args) {
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
+    @BeforeGroups (groups = {"testButton"})
+    public void beforeCheckButton(){
+        System.out.println("Check button");
+    }
+    @AfterGroups (groups = {"testButton"})
+    public void afterCheckButton(){
+        System.out.println("End Check button");
+    }
+
+    @Test (groups = {"testText"}, priority = 3)
+    public void checkTitlePage(){
+        Assert.assertEquals(driver.getTitle(),"aiTriage");
+    }
+
+    @Test (groups = {"testButton"}, priority = 2)
+    public void checkButtonDisplay(){
+        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"app\"]/div/form/div[4]")).isDisplayed(), true);
+
+    }
+
+    @Test (groups = {"testButton"}, enabled = true, description = "Test button 1",
+    expectedExceptions = AssertionError.class, priority = 1)
+    public void checkButtonSize(){
+        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"app\"]/div/form/div[4]")).getSize().width, 100);
+
     }
 
 }
